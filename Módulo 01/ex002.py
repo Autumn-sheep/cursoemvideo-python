4d0d4e86c1c93c1a8e31842ef8adcd51ad0363e7
Thiago Farias <196391699+krowemarf@users.noreply.github.com>
2025-06-15 13:07:25 -0300

    Primeira aula e Primeiros exercícios

---
+++ Módulo 01/ex002.py
@@ -0,0 +1,7 @@
+# Leia o dia, mês e ano de nascimento de uma pessoa
+# Escreva de maneira formatada
+dia = int(input('Dia:\n>'))
+mes = str(input('Mês:\n>'))
+ano = int(input('Ano:\n>'))
+
+print(f'Você nasceu no dia {dia} de {mes} de {ano}!')
