2c0f56453ba38c6f27681ccb4e7cabadd523fb8d
Thiago Farias <196391699+krowemarf@users.noreply.github.com>
2025-06-28 00:46:10 -0300

    Solução do exercício 007
    Mostrar o dobro, o triplo e a raiz quadrada de um número

---
+++ Módulo 01/exercícios/ex007.py
@@ -0,0 +1,7 @@
+# Leia um número e mostre o seu dobro, seu triplo e sua raiz quadrada
+num = int(input('Digite um número:\n> '))
+dobro = num * 2
+triplo = num * 3
+raiz_quadrada = num ** (1 / 2)
+
+print(f'Dobro: {dobro}\nTriplo: {triplo}\nRaiz quadrada: {raiz_quadrada:.2f}')
