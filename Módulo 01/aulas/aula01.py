dc517d140ade58bf62a623ede7af4aa63072e474
Thiago Farias <196391699+krowemarf@users.noreply.github.com>
2025-06-28 00:27:29 -0300

    Criar diretório para aulas
    Mover todas as aulas para o diretório "aulas"

---
+++ Módulo 01/aulas/aula01.py
@@ -0,0 +1,15 @@
+# Escrevendo uma mensagem
+print('Hello, world!')
+
+# Somando
+print(7 + 4)
+
+# Concatenando
+print('7' + '4')
+
+# Guardando valores
+nome = str(input('Nome:\n>'))
+idade = int(input('Idade:\n>'))
+nota = float(input('Nota:\n>'))
+
+print(f'Nome: {nome}\nIdade: {idade}\nNota: {nota:.2f}')
