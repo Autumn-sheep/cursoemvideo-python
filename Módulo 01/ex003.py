064aa92689269ec11814528b46c6e3df6d203883
Thiago Farias <196391699+krowemarf@users.noreply.github.com>
2025-06-28 00:31:35 -0300

    Remoção de arquivos desorganizados
    Arquivos de aulas e exercícios organizados nos próprios diretórios

--- Módulo 01/ex003.py
+++
@@ -1,6 +0,0 @@
-# Crie dois números e tente somar eles no final
-num1 = int(input('Digite um número:\n>'))
-num2 = int(input('Digite outro número:\n>'))
-res = num1 + num2
-
-print(f'A soma de {num1} e {num2} é igual a {res}')
--- Módulo 01/ex004.py
+++
@@ -1,6 +0,0 @@
-# Leia dois números e mostre a soma entre eles
-num1 = int(input('Digite um número: '))
-num2 = int(input('Digite outro número: '))
-soma = num1 + num2
-
-print(f'A soma de {num1} e {num2} é igual a {soma}.')
